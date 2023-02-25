c2fe07cc994362daf743d878cc8e94d4b01f10ed
nryabkov <78977205+nryabkovv@users.noreply.github.com>
2023-02-25 22:33:41 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -32,13 +32,11 @@ uint64_t nextPrime(uint64_t value) {
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-    uint64_t p = 0;
-    uint64_t summ = 2;
-    while (summ != hbound) {
-        if (checkPrime(summ) == true)
-            p += summ;
-        p += summ;
-        summ++;
+    uint64_t  summ = 0;
+    for (uint64_t i = 2; i < hbound; i++) {
+        if (checkPrime(i)) {
+            summ += i;
+        }
     }
-    return p;
+    return summ;
 }
