18ddd457c47907b9b7d6e21c47dcce8ff33ebd3a
nryabkov <78977205+nryabkovv@users.noreply.github.com>
2023-02-25 21:38:45 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,7 +5,7 @@
 
 
 bool checkPrime(uint64_t value) {
-    for (uint64_t i = 2; i <= sqrt(value); i++) {
+    for (uint64_t i = 2; i*i <= value; i++) {
         if (value % i != 0) {
             return true;
         }
